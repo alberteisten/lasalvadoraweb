c059fd1539f1307c22cb8488734dcca79ef6a6e3
alberto <albertovallejossirpa@gmail.com>
2020-01-05 22:35:35 -0400

    chat

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -38,6 +38,17 @@
     script(src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js')
     
     -->
+		<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'f3d24497c245d4362698b52812f14858aa4ca153';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
   </head>
   <body>
     <div class="mv-site">
